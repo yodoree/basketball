84ee1c349bd309294999dd8d15f9ed8f2d6f0788
hbmedi <info@hbmedi.kr>
2022-07-07 10:56:46 +0900

    5번째 연습경기

--- src/routes/Videos.tsx
+++ src/routes/Videos.tsx
@@ -234,6 +234,16 @@ function Videos() {
               </Link>
             </Video>
           </VideoContainer>
+          <VideoContainer>
+            <Video>
+              <Link
+                to={`/videos/${type}/2022/RpvqAgxwNak`}
+                state={{ name: "5번째 연습경기" }}
+              >
+                5번째 연습경기
+              </Link>
+            </Video>
+          </VideoContainer>
         </VideoList>
       ) : (
         <VideoList>
